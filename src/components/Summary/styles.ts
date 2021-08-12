e5647bc2d93f38c273a6dcdd276029cd9eeef865
Daniel <daniel.mileski@cidadania.gov.br>
2021-08-12 00:45:49 -0300

    Terminando capitulo 2

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  margin-top: -10rem;
+
+  div {
+    background: var(--shape);
+    padding: 1.5rem 2rem;
+    border-radius: 0.25rem;
+    color: var(--text-title);
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    strong {
+      display: block; // o padrão do strong é display inline. e nesse caso o margin=top não funciona
+      margin-top: 1rem; // o display block faz o strong funcionar como se fosse uma div, assim o margin-top funciona
+      font-size: 2rem;
+      font-weight: 500;
+      line-height: 3rem;
+    }
+
+    &.highlight-background {
+      background: var(--green);
+      color: #FFF;
+    }
+  }
+`;
